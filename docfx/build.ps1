99473fc16c450ee8baf5787eedab6c3a294a3206
Paul Bullock <paul.bullock@capacreative.co.uk>
2021-05-13 22:20:57 +0100

    minor build corrections

--- docfx/build.ps1
+++ docfx/build.ps1
@@ -7,4 +7,4 @@ docfx build ./main/docfx/docfx.json --warningsAsErrors $args
 
 # Copy the created site to the pnpcoredocs folder (= clone of the gh-pages branch)
 Remove-Item ./gh-pages/*/* -Recurse -Force
-copy-item -Force -Recurse ./main/docfx/_site/* -Destination ./gh-pages
+copy-item -Force -Recurse ./main/docfx/_site/* -Destination "./gh-pages"
