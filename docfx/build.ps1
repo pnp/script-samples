15fb5d82eb75054449bf234a04852914ad473481
Paul Bullock <paul.bullock@capacreative.co.uk>
2021-05-13 22:24:31 +0100

    disabled remove line from build script

--- docfx/build.ps1
+++ docfx/build.ps1
@@ -6,5 +6,5 @@ Set-StrictMode -Version 2.0
 docfx build ./main/docfx/docfx.json --warningsAsErrors $args
 
 # Copy the created site to the pnpcoredocs folder (= clone of the gh-pages branch)
-Remove-Item ./gh-pages/*/* -Recurse -Force
+#Remove-Item ./gh-pages/*/* -Recurse -Force
 copy-item -Force -Recurse ./main/docfx/_site/* -Destination "./gh-pages"
