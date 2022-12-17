8d67ed0839ff2baadb4ff557bcc6347dfcb8b2f2
Paul Bullock <paul.bullock@capacreative.co.uk>
2022-12-17 15:39:24 +0000

    Commit from GitHub Actions (Generate Site)

---
+++ assets/support-scripts/HappyHolidays-Community.ps1
@@ -0,0 +1,21 @@
+Write-Host @"
+
+
+
+   ██   ██  █████  ██████  ██████  ██    ██   
+   ██   ██ ██   ██ ██   ██ ██   ██  ██  ██   
+   ███████ ███████ ██████  ██████    ████    
+   ██   ██ ██   ██ ██      ██         ██     
+   ██   ██ ██   ██ ██      ██         ██     
+    
+                ██   ██  ██████  ██      ██ ██████   █████  ██    ██ ███████ 
+                ██   ██ ██    ██ ██      ██ ██   ██ ██   ██  ██  ██  ██      
+                ███████ ██    ██ ██      ██ ██   ██ ███████   ████   ███████ 
+                ██   ██ ██    ██ ██      ██ ██   ██ ██   ██    ██         ██ 
+                ██   ██  ██████  ███████ ██ ██████  ██   ██    ██    ███████ 
+
+   Thank you for EVERYTHING you do within the community! #CommunityRocks!                                                                                    
+
+"@
+
+# https://textkool.com/en/ascii-art-generator?hl=default&vl=default&font=ANSI%20Regular&text=%23Happy%20Holidays
