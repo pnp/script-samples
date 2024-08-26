1ee87250dac71517dd74e41c26bfec3f77a6f2cb
Kinga Kazala <22429087+kkazala@users.noreply.github.com>
2024-08-26 10:30:47 +0200

    readme for the Grant-APIPermissions_ManagedIdentity.ps1 script

--- scripts/aad-grant-serviceprincipal-api-permissions/Grant-APIPermissions_ManagedIdentity.ps1
+++ scripts/aad-grant-serviceprincipal-api-permissions/Grant-APIPermissions_ManagedIdentity.ps1
@@ -1,50 +1,69 @@
 <#
     .DESCRIPTION
+    This script can be used to grant System-Managed Identity used by automation (Azure Runbook, Azure Functions)
+    API permissions and access to SPO sites,that are necessary to:
+    - audit API permissions assigned to the "SharePoint Online Client Extensibility Web Application Principal".
+    - audit permissions requested by the installed SPFx solutions (tenant-level and site-level app catalogs)
+    - remove unused permissions.
+    
     The Set-ManagedIdentityAPIPermissions function grants the following roles to the Managed Identity used for automation:
-    - 'Application.Read.All',
-    - 'Sites.Selected' 
-    Once the 'Lists.SelectedOperations.Selected' is available productively, the 'Sites.Selected'  scope can be replaced
+        - 'Application.Read.All',
+        - 'Sites.Selected'     
+        - "DelegatedPermissionGrant.ReadWrite.All"
+    
+    Once the 'Lists.SelectedOperations.Selected' is available productively, the 'Sites.Selected' scope can be replaced.
 
-    The Set-SPOSiteAppCatalogPermissions function grants System-Assigned Managed Identity read access to 
+    The Set-SPOSiteAppCatalogPermissions function grants the Service Principal read access to 
     - root level SharePoint Site
     - tenant-level app catalog
     - sites with site-level app catalog
 
     .PARAMETER spId
-    Object Id of system-managed identity.
+    Object Id of System-Managed Identity.
     Navigate to the Azure Runbook, open Accoutn Setting group, select Identity and copy the Object (principal) ID value
 
-    .PARAMETER domainName
-
+    .PARAMETER tenantName
+    Tenant name is necessary to connect to the sharepoint site. 
 
     .NOTES
-    The function 'Set-APIPermissions' is based on https://github.com/gerickes/tutorial-managed-identity
-    and updated to use Microsoft Graph Powershell (https://learn.microsoft.com/en-us/powershell/microsoftgraph/azuread-msoline-cmdlet-map?view=graph-powershell-1.0)    
+    The script uses PnP.PowerShell and Microsoft.Graph. 
+    PnP PowerShell requires PowerShell 7.2 or later.
 
     Overview of Selected permissions in OneDrive and SharePoint: https://learn.microsoft.com/en-us/graph/permissions-selected-overview?tabs=powershell
     Assigning application permissions to lists, list items, folders, or files breaks inheritance on the assigned resource, 
     so be mindful of service limits for unique permissions in your solution design. Permissions at the site collection level do not break inheritance 
     because this is the root of permission inheritance.
 
-    Microsoft Graph Permissions Explorer: https://graphpermissions.merill.net/permission/
-
-    Lists and categorizes privilege for delegated permissions (OAuth2PermissionGrants) and application permissions (AppRoleAssignments).
-    Export-MsIdAppConsentGrantReport https://azuread.github.io/MSIdentityTools/commands/Export-MsIdAppConsentGrantReport
+    Other useful tools:
+    - Microsoft Graph Permissions Explorer: https://graphpermissions.merill.net/permission/
+    - Export-MsIdAppConsentGrantReport https://azuread.github.io/MSIdentityTools/commands/Export-MsIdAppConsentGrantReport
 
 #>
 param(
     [string]$spId,
     [string]$tenantName
 )
-Import-Module Microsoft.Graph.Authentication
-Import-Module Microsoft.Graph.Applications
+
     
 <#
     .DESCRIPTION
-    The Set-ManagedIdentityAPIPermissions function grants the following roles to the Managed Identity used for automation:
-    -  Microsoft Graph
+    The Set-ManagedIdentityAPIPermissions function grants the following Microsoft Graph API permissions:
+    - 'DelegatedPermissionGrant.ReadWrite.All'
     - 'Application.Read.All',
     - 'Sites.Selected'                     # once 'Lists.SelectedOperations.Selected' is released, scope can be changed
+
+    Other permissions may be added by updating the $permissionMap array. 
+    Use the following commands to retrieve information about additional applications: 
+    
+    Import-Module Microsoft.Graph.Applications
+    Connect-MgGraph -Scopes 'Application.Read.All'
+
+    Get-MgServicePrincipal -Search '"displayName:Team"' -CountVariable CountVar `
+         -Property "displayName,appId,replyUrls,servicePrincipalType,oauth2PermissionScopes,appRoles,resourceSpecificApplicationPermissions" `
+         -ConsistencyLevel eventual
+
+    See https://learn.microsoft.com/en-us/graph/api/resources/serviceprincipal?view=graph-rest-1.0#properties
+    for a currnet list of available properties and their descriptions
 
     .NOTES
     IMPORTANT: The DelegatedPermissionGrant.ReadWrite.All permission allows an app or a service to manage permission grants 
@@ -61,11 +80,12 @@ function Set-ManagedIdentityAPIPermissions {
     $permissionMap = @{
         '00000003-0000-0000-c000-000000000000' = @( # Microsoft Graph
             'Application.Read.All',
-            "DelegatedPermissionGrant.ReadWrite.All"
+            'DelegatedPermissionGrant.ReadWrite.All',
             'Sites.Selected'                     # once 'Lists.SelectedOperations.Selected' is released, scope can be changed
         )
     }
-
+    Import-Module Microsoft.Graph.Authentication
+    Import-Module Microsoft.Graph.Applications
     Connect-MgGraph  -Scopes "AppRoleAssignment.ReadWrite.All" , 'Application.Read.All'
 
     Get-MgServicePrincipal -All  | Where-Object { $_.AppId -in $permissionMap.Keys } -PipelineVariable SP | ForEach-Object {
@@ -87,12 +107,13 @@ function Set-ManagedIdentityAPIPermissions {
 
 
 <#
-The Set-SPOSiteAppCatalogPermissions function grants Managed Identity Read acess to the following SPO sites:
-- root site : this is required for the Azure Runbook to connect to SharePoint and request app catalogs
-- tenant level app catalog
-- all detected site level app catalogs
+    .DESCRIPTION
+    The Set-SiteAppCatalogPermissions function grants Managed Identity Read acess to the following SPO sites:
+    - root site : this is required for the Azure Runbook to connect to SharePoint and request app catalogs
+    - tenant level app catalog
+    - all detected site level app catalogs
 #>
-function Set-SPOSiteAppCatalogPermissions {
+function Set-SiteAppCatalogPermissions {
     param(
         [string]$tenantName,
         [string]$spId
@@ -116,5 +137,5 @@ function Set-SPOSiteAppCatalogPermissions {
 }
 
 Set-ManagedIdentityAPIPermissions -spId $spId 
-Set-SPOSiteAppCatalogPermissions -tenantName $tenantName -spId $spId 
+Set-SiteAppCatalogPermissions -tenantName $tenantName -spId $spId 
 
